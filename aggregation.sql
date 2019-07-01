64c4208485ceeeddf7c071d01b181884c625e453
Usman Iftikhar <muiftikhar85@gmail.com>
2019-06-30 19:47:47 -0500

    distinct added

--- aggregation.sql
+++ aggregation.sql
@@ -127,10 +127,47 @@ ORDER BY channel_count DESC;
 
 -- Are there any accounts associated with more than one region.
 
+SELECT 
+	a.id AS "Account ID", r.id as "Region ID",
+	a.name AS "Account Name", r.name AS "Region Name"
+FROM accounts a
+JOIN sales_reps sr
+ON sr.id = a.sales_rep_id
+JOIN region r
+ON r.id = sr.region_id;
+
+-- 351 rows affected.
+
+SELECT DISTINCT id, name
+FROM accounts;
+
+-- 351 rows affected.
+
+-- If each account was associated with more than one region, 
+-- the first query will return more rows than the second query.
 
 
 
 -- Have any sales reps worked on more than one account.
+
+SELECT sr.id, sr.name, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps sr
+ON sr.id = a.sales_rep_id
+GROUP BY sr.id, sr.name
+ORDER BY num_accounts;
+
+-- 50 rows affected.
+
+SELECT DISTINCT id, name
+FROM sales_reps;
+
+-- 50 rows affected.
+
+-- Actually all of the sales reps have worked on more than one account. 
+-- The fewest number of accounts any sales rep works on is 3. There are 50 sales reps, 
+-- and they all have more than one account. Using DISTINCT in the second query 
+-- assures that all of the sales reps are accounted for in the first query.
 
 
 
